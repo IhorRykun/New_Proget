d27f5a0b25845379f0525af96b7478d087387a30
IhorRykun <ihorrykun@gmail.com>
2023-03-17 16:28:56 +0000

    Deploying to gh-pages from @ IhorRykun/New_Projet@85f8b1da4e54e579d5d80928ef71fae0ebab717b 🚀

---
+++ index.7698b643.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".Problem__contain--svg"),o=document.querySelector(".problem__list");e.addEventListener("click",(function(){o.classList.toggle("none"),console.log("click")}));
+//# sourceMappingURL=index.7698b643.js.map
--- index.f0f5a539.js
+++
@@ -1,2 +0,0 @@
-var elem=document.querySelector(".Problem__contain--svg"),list=document.querySelector(".problem__list");function hendeTogle(){list.classList.toggle(".display "),console.log("click")}elem.addEventListener("click",hendeTogle);
-//# sourceMappingURL=index.f0f5a539.js.map
